0a0f6fdeffcc5c5344c696b89f12de736f645536
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-07 11:10:08 -0500

    Update main.lua

--- classjs/main.lua
+++ classjs/main.lua
@@ -1,7 +1,7 @@
 local Prototype = require("whereever prototype is");
 _G.Protos = {};
 
-local Class = require("Class.lua");
+local Class = require("Class.lua")(Prototype);
 local New = require("New.lua");
 
 return { Class, New };
