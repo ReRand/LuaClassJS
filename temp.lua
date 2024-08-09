f33129ebfffc94c2e9423b00882df0441b743237
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-09 17:10:53 -0500

    Update temp.lua

--- temp.lua
+++ temp.lua
@@ -1,6 +1,6 @@
 local config = {
-  coolPrintDebugClassStuff = false,
-  disableCoolPrint = true
+	coolPrintDebugClassStuff = false,
+	disableCoolPrint = true
 }
 
 local _, rbx = pcall(function() return not not game end);
@@ -19,24 +19,24 @@ local original_tostring = tostring;
 
 
 function Prototype:__tostring()
-  return "[class " .. self.__name .. "]";
+	return "[class " .. self.__name .. "]";
 end
 
 
 local function shallow(t)
-  local nt = {};
-  for k, v in pairs(t) do nt[k] = v end
-  return nt;
+	local nt = {};
+	for k, v in pairs(t) do nt[k] = v end
+	return nt;
 end
 
 
 local rawtostring = function (val)
-  local mt = getmetatable(val)
-  local __tostring = mt and mt.__tostring
-  if __tostring then mt.__tostring = nil end
-  local str = original_tostring(val)
-  if __tostring then mt.__tostring = __tostring end
-  return str
+	local mt = getmetatable(val)
+	local __tostring = mt and mt.__tostring
+	if __tostring then mt.__tostring = nil end
+	local str = original_tostring(val)
+	if __tostring then mt.__tostring = __tostring end
+	return str
 end
 
 
@@ -71,69 +71,69 @@ function dump(o, layer)
 	local lastt = pad("", "\t", layer-1);
 
 	if type(o) == 'table' and not o.__base then
-	  
-	  o = shallow(o);
-	  
-	  if o.__prototype then
-	    for k, v in pairs(o.__prototype) do o[k] = v end
-	  end
-	  
+
+		o = shallow(o);
+
+		if o.__prototype then
+			for k, v in pairs(o.__prototype) do o[k] = v end
+		end
+
 		local s = '{';
 
 		local li = 0;
 		local i = 0;
-		
+
 		local function dothe(k)
-		  return (k ~= "__instances" and (not string.match(k, "__") or config.coolPrintDebugClassStuff))
+			return (k ~= "__instances" and (not string.match(k, "__") or config.coolPrintDebugClassStuff))
 		end
 
 		for k, _ in pairs(o) do
-		  
-		  if dothe(k) then
-		    li = li + 1;
-		  end 
+
+			if dothe(k) then
+				li = li + 1;
+			end 
 		end
 
 		if li > 0 then
 			s = s.."\n";
 		end
-		
+
 		for k,v in pairs(o) do
-		  
-		  if dothe(k) then
-  			-- if not string.match(k, "__") then
-  			
-  			i = i + 1;
-  
-  			local e = "\n";
-  
-  			if i ~= li then
-  				e = ","..e;
-  			end
-  
-  			if type(k) ~= 'number' and string.match(k, " ") then k = '"'..k..'"' end;
-  			if type(v) == "string" then v = '"'..v..'"' end;
-  			if type(v) == "function" then
-  				local mName = fname(v);
-  
-  				if mName then
-  					v = mName .. "() (" .. fid(v) .. ")";
-  				else
-  					v = "function() (" .. fid(v) .. ")";
-  				end
-  			end
-  			
-  			if v ~= o then
-  			  s = s .. t .. k ..': ' .. dump(v, layer+1) .. e;
-  			end
-  			
-  			
-  			-- print(i, li);
-  			
-  			if i == li then
-  			  break;
-  			end
-			
+
+			if dothe(k) then
+				-- if not string.match(k, "__") then
+
+				i = i + 1;
+
+				local e = "\n";
+
+				if i ~= li then
+					e = ","..e;
+				end
+
+				if type(k) ~= 'number' and string.match(k, " ") then k = '"'..k..'"' end;
+				if type(v) == "string" then v = '"'..v..'"' end;
+				if type(v) == "function" then
+					local mName = fname(v);
+
+					if mName then
+						v = mName .. "() (" .. fid(v) .. ")";
+					else
+						v = "function() (" .. fid(v) .. ")";
+					end
+				end
+
+				if v ~= o then
+					s = s .. t .. k ..': ' .. dump(v, layer+1) .. e;
+				end
+
+
+				-- print(i, li);
+
+				if i == li then
+					break;
+				end
+
 			end
 		end
 
@@ -149,22 +149,22 @@ end
 
 
 function __prototostring(table)
-		local d = dump(table);
+	local d = dump(table);
 
-		if table.__name then
-			return table.__name .. " ("..tid(table)..") " .. d
-		end
+	if table.__name then
+		return table.__name .. " ("..tid(table)..") " .. d
+	end
 
-		return d;
+	return d;
 end
 
 
 function __protonewindex(table, key, value)
-	
+
 	if key == "constructor" then
 		rawset(table, "constructor", nil);
 	end
-	
+
 	if key == "__name" then
 		local oldName = rawget(table, "__name");
 
@@ -208,11 +208,11 @@ end
 
 
 function __metanewindex(table, key, value)
-	
+
 	if key == "constructor" then
 		rawset(table, "constructor", nil);
 	end
-	
+
 	if type(value) == "function" and not string.match(key, "__") then
 		table[key] = function(...)
 			return value(...);
@@ -281,7 +281,7 @@ function Prototype.new(name) return function(p)
 
 		p.__index = __protoindex;
 		p.__newindex = __protonewindex;
-		
+
 		if not rbx or not config.disableCoolPrint then
 			p.__tostring = dump;
 		end
@@ -290,8 +290,8 @@ function Prototype.new(name) return function(p)
 		self.__name = name;
 		self.__instances = {};
 		self.__prototype = proto;
-		
-		
+
+
 		function self.__prototype.__isa(n)
 			if type(n) == "table" then
 				return n.__name == self.__name;
@@ -299,14 +299,14 @@ function Prototype.new(name) return function(p)
 				return n == self.__name
 			end
 		end
-		
+
 
 		Prototype.__index = __protoindex;
 		Prototype.__newindex = __protonewindex;
 
 		self.__index = __protoindex;
 		self.__newindex = __protonewindex;
-		
+
 		--[[if not rbx then
 			self.__tostring = __prototostring(self);
 		end]]
@@ -348,33 +348,33 @@ function new(name) return (function(...)
 		if not c then
 			error("Class \""..name.."\" not found in globals while trying to create a new instance.");
 		end
-		
+
 		local exter = {
-		  __name = c.__name;
-		  __prototype = c.__prototype;
-		  __class = c
+			__name = c.__name;
+			__prototype = c.__prototype;
+			__class = c
 		};
-		
+
 		local ext = setmetatable(exter, c);
-		
+
 		function ext:__index(...)
-		  return __metaindex(self, ...);
+			return __metaindex(self, ...);
 		end
 		function ext:__newindex(...)
-		  return __metanewindex(self, ...);
+			return __metanewindex(self, ...);
 		end
-		
+
 		if not rbx or not config.disableCoolPrint then
-		  
-		  function ext:__tostring()
-		    return __prototostring(self);
-		  end
+
+			function ext:__tostring()
+				return __prototostring(self);
+			end
 		end
 
 		local self = setmetatable(exter, ext);
-		
+
 		self.__under = ext;
-		
+
 
 		if c.__instances then
 			-- table.insert(c.__instances, self);
@@ -396,76 +396,53 @@ end
 
 
 function getclass(n);
-  return _G.Protos[n];
+	return _G.Protos[n];
 end
 
 
 function isa(t, n)
-  return t.__isa(n);
+	return t.__isa(n);
 end
 
 
 function extend(nfrom) return (function(nto) return (function(p)
-  local base = _G.Protos[nfrom];
-  local proto = shallow(base.__prototype);
-  
-  for k, v in pairs(p) do
-    proto[k] = v;
-  end
-  
-  local self = Prototype.new(nto)(proto);
-  
-  for k, v in pairs(self.__prototype) do
-    if type(v) == "function" then
-      
+			local base = _G.Protos[nfrom];
+			local proto = shallow(base.__prototype);
+
+			for k, v in pairs(p) do
+				proto[k] = v;
+			end
+
+			local self = Prototype.new(nto)(proto);
+
+			for k, v in pairs(self.__prototype) do
+				if type(v) == "function" then
+
       --[[local f = v(self);
       
       print(f)]];
-      
+
       --[[v = function(...)
         return f(...);
       end
       
       self.__prototype[k] = v;]]
-    end
-  end
-  
-  self.__extendee = base;
-  
-  return self;
-end) end) end
+				end
+			end
+
+			self.__extendee = base;
+
+			return self;
+		end) end) end
 
 -- function new(name) return __new end
 
 
-class "Base" {
-  constructor = function(self, n)
-    self.Unusedname = n;
-  end,
-  
-  test = function(self)
-    return self.Username;
-  end,
-  
-  a = "b";
-};
-
-
-extend "Base" "User" {
-  constructor = function(self, n)
-    self.Username = n;
-  end
-}
-
-
-local user = new "User"('pim');
-
-
-print(user);
-
-
-return { class = class, new = new, extend = extend, getclass = getclass, isa = isa }
-
-
-
-
+local stuff = { class = class, new = new, extend = extend, getclass = getclass, isa = isa };
+return (function(...)
+	local args = {...};
+	for i, v in ipairs(args) do
+		args[i] = stuff[v];
+	end
+	return table.unpack(args);
+end)
