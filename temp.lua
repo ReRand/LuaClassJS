e3005ec458bd08d24b0df420ef727ca82e105b70
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-14 06:55:26 -0500

    Update temp.lua

--- temp.lua
+++ temp.lua
@@ -10,6 +10,10 @@ local Prototype = {};
 Prototype.__index = Prototype;
 
 
+local PrototypeItem = {};
+PrototypeItem.__index = PrototypeItem;
+
+
 
 local original_tostring = tostring;
 
@@ -17,6 +21,136 @@ local original_tostring = tostring;
 
 function Prototype:__tostring()
 	return "[class " .. self.__name .. "]";
+end
+
+
+function PrototypeItem.new(parent, value, t)
+	
+	if not t then t = {}; end;
+	
+	t.__value = value;
+	t.__parent = parent;
+	
+	if not t.__writable then t.__writable = true; end;
+	if not t.__configurable then t.__configurable = true; end;
+	if not t.__enumerable then t.__enumerable = true; end;
+	
+	function t.__iter(tbl)
+		print('a');
+		return tbl.__value;
+	end
+	
+	return setmetatable(t, PrototypeItem);
+end
+
+
+
+--[[function PrototypeItem.__tostring(tbl)
+	if type(tbl.__value) ~= "table" then
+		return 
+	else
+		return tostring(tbl.__value);
+	end
+end]]
+
+
+function PrototypeItem.__len(tbl)
+	
+	local i = 0;
+	
+	local success = pcall(function()
+		i = #tbl.__value;
+	end);
+	
+	if not success then
+		for _ in ipairs(tbl.__value) do i = i + 1; end
+	end
+	
+	return i;
+end
+
+
+function PrototypeItem.__iter(tbl)
+	return next, tbl.__value
+end
+
+
+
+function PrototypeItem.__le(tbl, b) return tbl.__value <= b; end
+function PrototypeItem.__lt(tbl, b) return tbl.__value < b; end
+function PrototypeItem.__ge(tbl, b) return tbl.__value >= b; end
+function PrototypeItem.__gt(tbl, b) return tbl.__value > b; end
+function PrototypeItem.__eq(tbl, b) return tbl.__value == b; end
+
+function PrototypeItem.__add(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value + b), tbl); end
+function PrototypeItem.__sub(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value - b), tbl); end
+function PrototypeItem.__mul(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value * b), tbl); end
+function PrototypeItem.__div(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value / b), tbl); end
+function PrototypeItem.__mod(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value * b), tbl); end
+function PrototypeItem.__pow(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value ^ b), tbl); end
+function PrototypeItem.__idiv(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value // b), tbl); end
+
+function PrototypeItem.__band(tbl, b) return PrototypeItem.new(tbl.__parent, (tbl.__value .. b), tbl); end
+function PrototypeItem.__unm(tbl) return PrototypeItem.new(tbl.__parent, (-tbl.__value), tbl); end;
+
+
+function PrototypeItem.__index(tbl, key)	
+	
+	print(tbl);
+	
+	-- nab from the item itself
+	if rawget(tbl, key) then
+		return rawget(tbl, key);
+		
+		
+	-- nab from the item's value
+	elseif pcall(function() return rawget(tbl.__value, key) end) then
+		return rawget(tbl.__value, key);
+		
+		
+	-- weird fix for it calling __metaindex on the parent table
+	elseif rawget(tbl, "__value") and type(tbl.__value) == "function" then
+		
+		local parent = rawget(tbl, "__parent") or tbl;
+		return rawget(tbl, "__value")(parent, key);
+	end
+end
+
+
+
+function PrototypeItem.__newindex(tbl, key, value)
+	-- nab from the item itself
+	if rawget(tbl, key) then
+		rawset(tbl, key, value);
+
+
+	-- nab from the item's value
+	elseif pcall(function() return rawget(tbl.__value, key) end) and tbl.__writable then
+		rawset(tbl.__value, key, value);
+		
+		
+	elseif pcall(function() return rawget(tbl.__value, key) end) and not tbl.__writable then
+		error("Class entry \"" .. key .. "\" not writeable");
+	end
+end
+
+
+function PrototypeItem.__call(tbl, ...)
+	
+	print(tbl);
+	
+	for k, v in pairs({...}) do
+		
+		if type(v) == "table" then
+			print(v, v.__name, tbl.__parent.__name)
+		end
+		
+		if type(v) == "table" and v.__name and v.__name == tbl.__parent.__name then
+			return rawget(tbl, "__value")(...);
+		end
+	end
+	
+	return rawget(tbl, "__value")(rawget(tbl, "__parent"), ...);
 end
 
 
@@ -145,94 +279,105 @@ local function dump(o, layer)
 end
 
 
-local function __prototostring(table)
-	local d = dump(table);
+local function __prototostring(tbl)
+	local d = dump(tbl);
 
-	if table.__name then
-		return table.__name .. " ("..tid(table)..") " .. d
+	if tbl.__name then
+		return tbl.__name .. " ("..tid(tbl)..") " .. d
 	end
 
 	return d;
 end
 
 
-local function __protonewindex(table, key, value)
+local function __protonewindex(tbl, key, value)
+
+	local output = nil;
 
 	if key == "constructor" then
-		rawset(table, "constructor", nil);
+		return rawset(tbl, "constructor", nil);
 	end
 
 	if key == "__name" then
-		local oldName = rawget(table, "__name");
+		local oldName = rawget(tbl, "__name");
 
 		if oldName then
 			_G.Protos[oldName] = nil;
 		end
 
-		_G.Protos[value] = table;
-
-		return rawset(table, "__name", value);
+		_G.Protos[value] = tbl;
+		
+		
+		return rawset(tbl, "__name", PrototypeItem.new(tbl, value));
 	end
 
 
 	if type(value) == "function" and not string.match(key, "__") then
-		table.__prototype[key] = function(...)
+		tbl.__prototype[key] = PrototypeItem.new(tbl, function(...)
 			return value(...);
-		end
+		end)
 
+	elseif not string.match(key, "__") then
+		local item = PrototypeItem.new(tbl, value);
+		return rawset(tbl, key, item);
+	
 	else
-		return rawset(table, key, value);
+		return rawset(tbl, key, value);
 	end
 end
 
 
-local function __protoindex(table, key)
+local function __protoindex(tbl, key)
+	local protoGot = rawget(tbl.__prototype, key);
 	
-	local protoGot = rawget(table.__prototype, key);
-	
-	if protoGot and not rawget(table, key) then
+	if protoGot and not rawget(tbl, key) then
 
 		if type(protoGot) == "function" then
 			return function(...) 
-				return protoGot(table, ...);
+				return protoGot(tbl, ...);
 			end
 		else
 			return protoGot;
 		end
 
-
 	else
-		return rawget(table, key);
+		return rawget(tbl, key);
 	end
 end
 
 
-local function __metanewindex(table, key, value)
+local function __metanewindex(tbl, key, value)
 
 	if key == "constructor" then
-		rawset(table, "constructor", nil);
+		rawset(tbl, "constructor", nil);
 	end
 
 	if type(value) == "function" and not string.match(key, "__") then
-		table[key] = function(...)
+		tbl[key] = PrototypeItem.new(tbl, function(...)
 			return value(...);
-		end
+		end)
+
+	elseif not string.match(key, "__") then
+		local item = PrototypeItem.new(tbl, value);
+		return rawset(tbl, key, item);
 
 	else
-		return rawset(table, key, value);
+		return rawset(tbl, key, value);
 	end
 end
 
 
-local function __metaindex(table, key)
+local function __metaindex(tbl, key)
 	
-	local protoGot = rawget(table.__prototype, key);
+	local protoGot = rawget(tbl.__prototype, key);
+	local normalGot = rawget(tbl, key);
+	local self = rawget(tbl, "__parent") or tbl;
 	
-	if protoGot and not rawget(table, key) then
+	if protoGot and not rawget(tbl, key) then
 
 		if type(protoGot) == "function" then
 			return function(...) 
-				return protoGot(table, ...);
+				return protoGot(self, ...);
 			end
 		else
 			return protoGot
@@ -240,7 +385,14 @@ local function __metaindex(table, key)
 
 
 	else
-		return rawget(table, key);
+		
+		if type(normalGot) == "function" then
+			return function(...)
+				return normalGot(self, ...);
+			end
+		else
+			return normalGot;
+		end
 	end
 end
 
@@ -276,9 +428,9 @@ function Prototype.new(name) return function(p)
 		Prototype.__index = Prototype;
 
 		local proto = {};
-
+		
 		for k, v in pairs(p) do
-			proto[k] = v;
+			proto[k] = PrototypeItem.new(nil, v);
 			p[k] = nil;
 		end
 
@@ -293,13 +445,19 @@ function Prototype.new(name) return function(p)
 		self.__name = name;
 		self.__instances = {};
 		self.__prototype = proto;
+		
+		for k, v in pairs(proto) do
+			if type(v) == "table" then
+				rawset(v, "__parent", self);
+			end
+		end
 
 
-		function self.__prototype.__isa(n)
+		function self.__prototype.__isa(self, n)
 			if type(n) == "table" then
 				return n.__name == self.__name;
 			else
-				return n == self.__name
+				return n == self.__name;
 			end
 		end
 
@@ -316,7 +474,7 @@ function Prototype.new(name) return function(p)
 
 
 		if not rawget(self, "constructor") and not rawget(self.__prototype, "constructor") then
-			function self.__prototype.constructor() end
+			self.__prototype.constructor = PrototypeItem.new(self, function() end);
 
 
 		elseif rawget(self, "constructor") or rawget(self.__prototype, "constructor") then
@@ -331,9 +489,9 @@ function Prototype.new(name) return function(p)
 			rawset(self, "constructor", nil);
 			rawset(self.__prototype, "constructor", nil);
 
-			function self.__prototype.constructor(...)
+			self.__prototype.constructor = PrototypeItem.new(self, function(...)
 				return constructor(...);
-			end
+			end)
 		end
 
 
@@ -348,9 +506,11 @@ function new(name) return (function(...)
 
 		local c = _G.Protos[name];
 
+		-- if it can't find it in _G.Protos
 		if not c then
 			error("Class \""..name.."\" not found in globals while trying to create a new instance.");
 		end
+
 
 		local exter = {
 			__name = c.__name;
@@ -358,14 +518,19 @@ function new(name) return (function(...)
 			__class = c
 		};
 
+
 		local ext = setmetatable(exter, c);
+		
+
 
 		function ext:__index(...)
 			return __metaindex(self, ...);
 		end
+
 		function ext:__newindex(...)
 			return __metanewindex(self, ...);
 		end
+
 
 		if not rbx or not config.disableCoolPrint then
 
@@ -373,6 +538,7 @@ function new(name) return (function(...)
 				return __prototostring(self);
 			end
 		end
+
 
 		local self = setmetatable(exter, ext);
 
@@ -382,9 +548,15 @@ function new(name) return (function(...)
 		if c.__instances then
 			-- table.insert(c.__instances, self);
 		end
+		
+		for k, v in pairs(self.__prototype) do
+			if type(v) == "table" then
+				rawset(v, "__parent", self);
+			end
+		end
 
-		local ret = rawget(self.__prototype, "constructor")(self, ...);
-
+		local ret = self.__prototype.constructor(self, ...);
+		
 		if not ret then
 			return self;
 		else
@@ -419,6 +591,11 @@ function extend(nfrom) return (function(nto) return (function(p)
 			local self = Prototype.new(nto)(proto);
 
 			for k, v in pairs(self.__prototype) do
+				
+				if type(v) == "table" and rawget(v, "__parent") then
+					rawset(v, "__parent", self);
+				end
+				
 				if type(v) == "function" then
 
       --[[local f = v(self);
@@ -443,6 +620,7 @@ function extend(nfrom) return (function(nto) return (function(p)
 
 
 local stuff = { class = class, new = new, extend = extend, getclass = getclass, isa = isa };
+
 
 return (function(...)
 	local args = {...};
