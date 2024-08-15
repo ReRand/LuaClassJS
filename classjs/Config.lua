5d576cfef5c4e15a94c537ee8f0612ad3874ceee
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 10:04:37 -0500

    Update Config.lua

--- classjs/Config.lua
+++ classjs/Config.lua
@@ -1,8 +1,10 @@
 return {
-	
-  coolPrintDebugClassStuff = false,
-	disableInnerCoolPrint = false,
-	disableCoolPrint = true,
-	useObjects = false
 
+	
+coolPrintDebugClassStuff = false, 	-- if the cool print should include debug class things such as __ functions
+disableInnerCoolPrint = false,    	-- if PropertyItem's cool print should be disabled
+disableCoolPrint = true,		-- if instances' cool print should be disabled
+useObjects = false			-- if it should use PropertyItem and object stuff, if false it just sets the property's value
+
+	
 };
