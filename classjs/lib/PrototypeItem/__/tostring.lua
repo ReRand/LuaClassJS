35d7ae385dd853b8b1bad91d174f531c86a4c824
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 11:55:12 -0500

    Update tostring.lua

--- classjs/lib/PrototypeItem/__/tostring.lua
+++ classjs/lib/PrototypeItem/__/tostring.lua
@@ -3,8 +3,8 @@ return function(p, payload, PrototypeItem)
   function PrototypeItem.__tostring(tbl)
   	if type(tbl.__value) == "table" and not p.Config.disableInnerCoolPrint then
   		return p.dump(tbl.__value);
-  	elseif type(tbl.__value) == "table" and p.Config.disableInnerCoolPrint and baseprint then
-  			return baseprint(tbl.__value)
+  	elseif type(tbl.__value) == "table" and p.Config.disableInnerCoolPrint and p.baseprint then
+  			return p.baseprint(tbl.__value)
   	else
   		return tostring(tbl.__value);
   	end
