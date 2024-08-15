a9a6b995315dd60458e45a2f5af13c4b112bdf0b
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 08:02:00 -0500

    Update init.lua

--- classjs/init.lua
+++ classjs/init.lua
@@ -1,7 +1,16 @@
+local _, rbx = pcall(function() return not not game end);
+
 local Prototype = require("whereever prototype is");
 _G.Protos = {};
 
 local Class = require("Class.lua")(Prototype);
 local New = require("New.lua");
+
+local config = {
+	coolPrintDebugClassStuff = false,
+	disableInnerCoolPrint = false,
+	disableCoolPrint = true,
+	useObjects = false
+};
 
 return { Class, New };
