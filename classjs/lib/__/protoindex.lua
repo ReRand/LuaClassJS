77658fe5c2e88f615382b3440f4513382751f0bb
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 08:25:35 -0500

    Create protoindex.lua

---
+++ classjs/lib/__/protoindex.lua
@@ -0,0 +1,21 @@
+return function(payload)
+
+  function __protoindex(tbl, key)
+  	local protoGot = rawget(tbl.__prototype, key);
+  	
+  	if protoGot and not rawget(tbl, key) then
+  
+  		if type(protoGot) == "function" then
+  			return function(...) 
+  				return protoGot(tbl, ...);
+  			end
+  		else
+  			return protoGot;
+  		end
+  
+  	else
+  		return rawget(tbl, key);
+  	end
+  end
+
+end
