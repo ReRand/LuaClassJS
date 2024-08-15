3d6d994523396eb79a715c0c6f1bf5964a95d230
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 08:21:27 -0500

    Update new.lua

--- classjs/lib/new.lua
+++ classjs/lib/new.lua
@@ -1,23 +1,67 @@
-return function New(name) return function(...)
+return function(payload)
+
+    return function new(name) return (function(...)
+
+		local c = payload.Protos[name];
+
+		-- if it can't find it in _G.Protos
+		if not c then
+			error("Class \""..name.."\" not found in globals while trying to create a new instance.");
+		end
+
+
+		local exter = {
+			__name = c.__name;
+			__prototype = c.__prototype;
+			__class = c
+		};
+
+
+		local ext = setmetatable(exter, c);
+		
+
+
+		function ext:__index(...)
+			return payload.__metaindex(self, ...);
+		end
+
+		function ext:__newindex(...)
+			return payload.__metanewindex(self, ...);
+		end
+
+
+		if not payload.rbx or not payload.Config.disableCoolPrint then
+
+			function ext:__tostring()
+				return payload.__prototostring(self);
+			end
+		end
+
+
+		local self = setmetatable(exter, ext);
+
+		-- self.__under = ext;
+
+
+		if c.__instances then
+			-- table.insert(c.__instances, self);
+		end
+		
+		if payload.Config.useObjects then
+  		for k, v in pairs(self.__prototype) do
+  			if type(v) == "table" then
+  				rawset(v, "__parent", self);
+  			end
+  		end
+  	end
+
+		local ret = self.__prototype.constructor(self, ...);
+		
+		if not ret then
+			return self;
+		else
+			return ret;
+		end
+	end) end
     
-  local c = _G.Protos[name];
-  
-  local self = setmetatable({}, c);
-  
-  self.Name = c.Name;
-  self.Prototype = c.Prototype;
-  
-  self.__index = __realindex;
-  self.__newindex = __realnewindex;
-  
-  table.insert(c.Instances, self);
-  
-  local ret = rawget(self.Prototype, "Constructor")(self, ...);
-  
-  if not ret then
-    return self;
-  else
-    return ret;
-  end
-    
-end end
+end
