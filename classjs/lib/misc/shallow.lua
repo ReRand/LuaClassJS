36a515248a3ff852e26e876ec493eaef1581397b
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 09:23:50 -0500

    Update shallow.lua

--- classjs/lib/misc/shallow.lua
+++ classjs/lib/misc/shallow.lua
@@ -1,9 +1,11 @@
 return function(payload)
 
-  return function shallow(t)
+  function shallow(t)
   	local nt = {};
   	for k, v in pairs(t) do nt[k] = v end
   	return nt;
   end
+
+  return shallow;
 
 end
