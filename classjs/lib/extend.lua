5fe9e69485abb30507616df526ef3519bb8e3e7e
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 13:59:10 -0500

    Create extend.lua

---
+++ classjs/lib/extend.lua
@@ -0,0 +1,43 @@
+return function(payload)
+
+  function extend(nfrom) return (function(nto) return (function(p)
+			local base = payload.Protos[nfrom];
+			local proto = payload.shallow(base.__prototype);
+
+			for k, v in pairs(p) do
+				proto[k] = v;
+			end
+
+			local self = payload.Prototype.new(nto)(proto);
+
+			function self.__prototype.__super(...)
+				local constructor = rawget(base, "constructor") and rawget(base, "constructor") or rawget(base.__prototype, "constructor");
+				return constructor(...);
+			end
+
+			for k, v in pairs(self.__prototype) do
+				
+				if type(v) == "table" and rawget(v, "__parent") then
+					rawset(v, "__parent", self);
+				end
+				
+				if type(v) == "function" then
+
+      --[[local f = v(self);
+      
+      print(f)]]
+
+      --[[v = function(...)
+        return f(...);
+      end
+      
+      self.__prototype[k] = v;]]
+				end
+			end
+
+			self.__extendee = base;
+
+			return self;
+		end) end) end
+  
+end
