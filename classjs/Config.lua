b0bf84bc1fe55f93701693be0d6ea03d53389450
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 10:09:06 -0500

    Update Config.lua

--- classjs/Config.lua
+++ classjs/Config.lua
@@ -1,10 +1,16 @@
+--[[ 
+NOTE: cool print is some methods used to make printing on base lua prettier
+if you're using this on roblox it should be disabled anyway
+]]
+
+
 return {
 
 	
 coolPrintDebugClassStuff = false, 	-- if the cool print should include debug class things such as __ functions
 disableInnerCoolPrint = false,    	-- if PropertyItem's cool print should be disabled
-disableCoolPrint = true,		-- if instances' cool print should be disabled
-useObjects = false			-- if it should use PropertyItem and object stuff, if false it just sets the property's value
-
+disableCoolPrint = false,		-- if instances' cool print should be disabled
+useObjects = false,			-- if it should use PropertyItem and object stuff, if false it just sets the property's value
+forceRbx = nil				-- if roblox features should be forced, change from nil to use this
 	
 };
