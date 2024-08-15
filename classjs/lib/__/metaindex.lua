6787bf968badf8f92bf04ee996c77bce71de2aa3
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 08:28:10 -0500

    Create metaindex.lua

---
+++ classjs/lib/__/metaindex.lua
@@ -0,0 +1,31 @@
+return function(payload)
+  
+  return function __metaindex(tbl, key)
+  	local protoGot = rawget(tbl.__prototype, key);
+  	local normalGot = rawget(tbl, key);
+  	local self = rawget(tbl, "__parent") or tbl;
+  	
+  	if protoGot and not rawget(tbl, key) then
+  
+  		if type(protoGot) == "function" then
+  			return function(...) 
+  				return protoGot(self, ...);
+  			end
+  		else
+  			return protoGot
+  		end
+  
+  
+  	else
+  		
+  		if type(normalGot) == "function" then
+  			return function(...)
+  				return normalGot(self, ...);
+  			end
+  		else
+  			return normalGot;
+  		end
+  	end
+  end
+  
+end
