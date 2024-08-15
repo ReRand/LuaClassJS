f63a635d40c40a1a951b4a8dca4fed4237f6d44c
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 08:27:17 -0500

    Create metanewindex.lua

---
+++ classjs/lib/__/metanewindex.lua
@@ -0,0 +1,35 @@
+return function(payload)
+  
+  function __metanewindex(tbl, key, value)
+  	if key == "constructor" then
+  		rawset(tbl, "constructor", nil);
+  	end
+  
+  	if type(value) == "function" and not string.match(key, "__") then
+  		if payload.Config.useObjects then
+  			tbl[key] = payload.PrototypeItem.new(tbl, function(...)
+  				return value(...);
+  			end)
+  		else
+  			tbl[key] = function(...)
+  				return value(...);
+  			end
+  		end
+  
+  	elseif not string.match(key, "__") then
+  
+  		local item;
+  
+  		if payload.Config.useObjects then
+  			item = payload.PrototypeItem.new(tbl, value);
+  		else
+  			item = value;
+  		end
+  		return rawset(tbl, key, item);
+  
+  	else
+  		return rawset(tbl, key, value);
+  	end
+  end
+  
+end
