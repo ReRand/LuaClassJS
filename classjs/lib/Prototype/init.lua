954ff7be9575602a64780b8924f33f95ee827834
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 12:08:13 -0500

    Update init.lua

--- classjs/lib/Prototype/init.lua
+++ classjs/lib/Prototype/init.lua
@@ -1,6 +1,25 @@
-local Prototype = {};
-Prototype.__index = Prototype;
+return function(payload)
 
-require("")
+  local rbx = payload.rbx;
 
-return Prototype;
+  local Prototype = {};
+  Prototype.__index = Prototype;
+
+  local typePayload = {
+
+    -- methods
+    new = rbx and script.methods.new or "methods/new.lua", 			             -- creates a new Prototype
+
+    
+    -- internal metatable functions
+    __tostring = rbx and script.__.tostring or "__/tostring.lua", 			     -- controls what the PrototypeItem outputs
+    
+  }
+
+  for k, v in pairs(typePayload) do
+  	require(v)(payload, typePayload, Prototype);
+  end
+
+  return Prototype;
+  
+end
