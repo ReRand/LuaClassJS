57506c0ee0a14854f178cd6032b78626ff5a6801
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 11:33:29 -0500

    Update init.lua

--- classjs/lib/PrototypeItem/init.lua
+++ classjs/lib/PrototypeItem/init.lua
@@ -6,6 +6,10 @@ return function(payload)
   PrototypeItem.__index = PrototypeItem;
 
   local typePayload = {
+
+    -- methods
+    new = rbx and script.methods.new or "methods/new.lua", 			             -- creates a new PrototypeItem
+    __tostring = rbx and script.__.tostring or "__/tostring.lua", 			     -- creates a new PrototypeItem
     
   }
 
