3f5917340df2834a70b43e2f6693c6de0eb4a1b4
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 08:31:57 -0500

    Create rawtostring.lua

---
+++ classjs/lib/misc/rawtostring.lua
@@ -0,0 +1,14 @@
+local original_tostring = tostring;
+
+return function(payload)
+  
+  function rawtostring(val)
+  	local mt = getmetatable(val)
+  	local __tostring = mt and mt.__tostring
+  	if __tostring then mt.__tostring = nil end
+  	local str = original_tostring(val)
+  	if __tostring then mt.__tostring = __tostring end
+  	return str
+  end
+
+end
