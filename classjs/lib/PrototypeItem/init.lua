1ddd644e6c1f0d75c3dcfbfa2784860a33e2cac9
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-15 11:27:08 -0500

    Update init.lua

--- classjs/lib/PrototypeItem/init.lua
+++ classjs/lib/PrototypeItem/init.lua
@@ -1,1 +1,14 @@
+return function(payload)
 
+  local rbx = payload.rbx;
+
+  local PrototypeItem = {};
+  PrototypeItem.__index = PrototypeItem;
+
+  local typePayload = {
+    
+  }
+
+  return PrototypeItem;
+  
+end
