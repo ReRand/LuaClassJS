df8287d38de06513d6980da3dfc4d4ecbd475132
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-08-16 09:21:15 -0500

    Update Config.lua

--- classjs/Config.lua
+++ classjs/Config.lua
@@ -9,7 +9,7 @@ return {
 	
 coolPrintDebugClassStuff = false, 	-- if the cool print should include debug class things such as __ functions
 disableInnerCoolPrint = false,    	-- if PropertyItem's cool print should be disabled
-disableCoolPrint = false,		-- if instances' cool print should be disabled
+disableCoolPrint = true,		-- if instances' cool print should be disabled
 useObjects = false,			-- if it should use PropertyItem and object stuff, if false it just sets the property's value
 forceRbx = nil				-- if roblox features should be forced, change from nil to use this
 	
